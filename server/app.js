227aa75272bfd0c2b7bacd47ce39dd89a0a6236b
imrancodingschool <imrancodingshool@gmail.com>
2025-10-06 00:42:00 +0530

    added new feature

--- server/app.js
+++ server/app.js
@@ -7,23 +7,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  process.env.CLIENT_URL, // your deployed client
-  "http://localhost:5173" // your local client
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(cors({origin:"https://imran-shop.vercel.app",credentials:true}))
 
 app.use(cookieParser());
 app.use(express.json());
